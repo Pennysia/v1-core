1e382682df06bde49835b4076c740bf6647a4a7f
Settee <ake14243@gmail.com>
2025-08-15 16:05:44 +0700

    add: validation library with self-call protection and improved error handling

--- src/libraries/Validation.sol
+++ src/libraries/Validation.sol
@@ -3,8 +3,13 @@ pragma solidity 0.8.28;
 
 library Validation {
     error tokenError();
+    error selfCall();
 
     function checkTokenOrder(address token0, address token1) internal pure {
         require(token0 < token1, tokenError());
+    }
+
+    function notThis(address input) internal view {
+        require(input != address(this), selfCall());
     }
 }
