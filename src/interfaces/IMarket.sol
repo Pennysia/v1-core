b858f8d18241859aef75859899a8354805f2b3e6
Settee <ake14243@gmail.com>
2025-08-15 16:05:38 +0700

    add: market interface events and error handling for pool operations

--- src/interfaces/IMarket.sol
+++ src/interfaces/IMarket.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.28;
 
 interface IMarket {
     error forbidden();
+    error minimumLiquidity();
 
     struct Pair {
         uint128 reserve0Long;
@@ -13,4 +14,7 @@ interface IMarket {
         uint64 blockTimestampLast;
         uint192 cbrtPriceX128CumulativeLast; // cum. of cbrt(y/x * 10^128)*timeElapsed
     }
+
+    event Create(address indexed token0, address indexed token1, uint256 pairId);
+    event Mint(address indexed sender, address indexed to, uint256 indexed pairId, uint256 amount0, uint256 amount1);
 }
