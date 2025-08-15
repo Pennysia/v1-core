2509f13cb90716472555be6e34b1540fcdcfea80
Settee <ake14243@gmail.com>
2025-08-15 16:05:49 +0700

    add: payment interface for token request callbacks in pool operations

---
+++ src/interfaces/IPayment.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.28;
+
+/// @title IPayment
+///@notice these are payment request callbacks covering mint, burn, flash, and swap
+///@dev for direct pool interaction, this interface is not needed. see [Market.sol] for more details.
+interface IPayment {
+    /// @notice this is used in createLiquidity, flash, and swap
+    /// the caller need to pays specific amounts of tokens to the Market contract.
+    function requestToken(address to, address[] memory tokens, uint256[] memory paybackAmounts) external payable;
+}
