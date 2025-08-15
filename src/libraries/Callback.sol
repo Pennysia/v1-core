1f0b96036b13b2e65fe23a1589813c516229bfb9
Settee <ake14243@gmail.com>
2025-08-15 16:05:52 +0700

    add: callback library for token payment verification and pool interaction

---
+++ src/libraries/Callback.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.28;
+
+import {IPayment} from "../interfaces/IPayment.sol";
+import {ILiquidity} from "../interfaces/ILiquidity.sol";
+import {PairLibrary} from "./PairLibrary.sol";
+
+library Callback {
+    error InsufficientPayback();
+
+    function tokenCallback(
+        address caller,
+        address to,
+        address[] memory tokens,
+        uint256[] memory balancesBefore,
+        uint256[] memory paybackAmounts
+    ) internal {
+        uint256 len = tokens.length;
+        IPayment(caller).requestToken(to, tokens, paybackAmounts); // user paybacks
+
+        // Verify payback amounts for each token
+        for (uint256 i = 0; i < len; i++) {
+            uint256 paid = PairLibrary.getBalance(tokens[i]) - balancesBefore[i];
+            require(paid >= paybackAmounts[i], InsufficientPayback());
+        }
+    }
+}
