4cd595e0de3523862a7a80652b44e739416e9008
Settee <ake14243@gmail.com>
2025-08-16 19:56:38 +0700

    feat: expand validation library with comprehensive checks
    - Add equalLengths function for array length validation
    - Add notZero function for non-zero value checks
    - Add checkUnique function for duplicate token validation
    - Add new error types: lengthError, zeroValue, duplicatedToken
    - Enhance validation capabilities for market operations

--- src/libraries/Validation.sol
+++ src/libraries/Validation.sol
@@ -3,13 +3,32 @@ pragma solidity 0.8.28;
 
 library Validation {
     error tokenError();
+    error lengthError();
     error selfCall();
+    error zeroValue();
+    error duplicatedToken();
 
     function checkTokenOrder(address token0, address token1) internal pure {
         require(token0 < token1, tokenError());
     }
 
+    function equalLengths(uint256 length0, uint256 length1) internal pure {
+        require(length0 == length1, lengthError());
+    }
+
     function notThis(address input) internal view {
         require(input != address(this), selfCall());
+    }
+
+    function notZero(uint256 input) internal pure {
+        require(input > 0, zeroValue());
+    }
+
+    function checkUnique(address[] memory tokens) internal pure {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            for (uint256 j = i + 1; j < tokens.length; j++) {
+                require(tokens[i] != tokens[j], duplicatedToken());
+            }
+        }
     }
 }
