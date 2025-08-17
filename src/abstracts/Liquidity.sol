fd6abbcc6fb555316d8be03fa2436b20e2a231cc
Settee <ake14243@gmail.com>
2025-08-17 16:11:19 +0700

    docs(Liquidity): add NatSpec and brief comments
    Describe TTL allowances, EIP-2612 domain, transfer/permit/mint/burn behavior. No logic changes.

--- src/abstracts/Liquidity.sol
+++ src/abstracts/Liquidity.sol
@@ -52,21 +52,29 @@ abstract contract Liquidity is ILiquidity, Deadline {
     //////////////////////////////////////////////////////////////*/
 
     /// @notice Returns the name of the token
+    /// @return The name of the token
     function name() public pure override returns (string memory) {
         return "Pennysia Liquidity";
     }
 
     /// @notice Returns the symbol of the token
+    /// @return The symbol of the token
     function symbol() public pure override returns (string memory) {
         return "Pennysia-LP";
     }
 
     /// @notice Returns the number of decimals used by the token
+    /// @return The number of decimals
     function decimals() public pure override returns (uint8) {
         return 18;
     }
 
     /// @notice Returns the total supply of LP tokens
+    /// @param poolId The poolId of the token
+    /// @return longX Amount of long position X tokens
+    /// @return shortX Amount of short position X tokens
+    /// @return longY Amount of long position Y tokens
+    /// @return shortY Amount of short position Y tokens
     function totalSupply(uint256 poolId)
         public
         view
@@ -81,6 +89,13 @@ abstract contract Liquidity is ILiquidity, Deadline {
         );
     }
 
+    /// @notice Returns the LP token balance of an account
+    /// @param account The address to query the balance of
+    /// @param poolId The poolId of the token
+    /// @return longX Amount of long position X tokens
+    /// @return shortX Amount of short position X tokens
+    /// @return longY Amount of long position Y tokens
+    /// @return shortY Amount of short position Y tokens
     function balanceOf(address account, uint256 poolId)
         public
         view
@@ -96,6 +111,10 @@ abstract contract Liquidity is ILiquidity, Deadline {
     }
 
     /// @notice Approves or revokes permission for a spender to transfer tokens
+    /// @param spender The address to approve or revoke permission for
+    /// @param poolId The poolId of the token
+    /// @param value The block timestamp limit
+    /// @return A boolean indicating whether the operation succeeded
     function approve(address spender, uint256 poolId, uint256 value) public override returns (bool) {
         address owner = msg.sender;
         allowance[owner][spender][poolId] = value;
@@ -105,21 +124,28 @@ abstract contract Liquidity is ILiquidity, Deadline {
 
     /// @notice Transfers LP tokens to another address
     /// @dev if 'to' is the address(0), it will burn the tokens instead of transferring.
+    /// @param to The address to transfer tokens to
+    /// @param poolId The poolId of the token
+    /// @param longX The amount of long position X tokens to transfer
+    /// @param shortX The amount of short position X tokens to transfer
+    /// @param longY The amount of long position Y tokens to transfer
+    /// @param shortY The amount of short position Y tokens to transfer
+    /// @return A boolean indicating whether the transfer succeeded
     function transfer(address to, uint256 poolId, uint128 longX, uint128 shortX, uint128 longY, uint128 shortY)
         public
         returns (bool)
     {
         require(to != address(this), InvalidAddress());
         if (to != address(0)) {
+            // Update sender's balance.
             LpInfo storage lpInfo_from = _balances[msg.sender][poolId];
             lpInfo_from.longX -= longX;
             lpInfo_from.shortX -= shortX;
             lpInfo_from.longY -= longY;
             lpInfo_from.shortY -= shortY;
 
+            // Update recipient's balance (unchecked for overflow safety as sums can't exceed max).
             LpInfo storage lpInfo_to = _balances[to][poolId];
-            // Cannot overflow because the sum of all user
-            // balances can't exceed the max uint256 value.
             unchecked {
                 lpInfo_to.longX += longX;
                 lpInfo_to.shortX += shortX;
@@ -128,6 +154,7 @@ abstract contract Liquidity is ILiquidity, Deadline {
             }
             emit Transfer(msg.sender, to, poolId, longX, shortX, longY, shortY);
         } else {
+            // Burn instead of transfer.
             _burn(msg.sender, poolId, longX, shortX, longY, shortY);
         }
         return true;
@@ -135,6 +162,14 @@ abstract contract Liquidity is ILiquidity, Deadline {
 
     /// @notice Transfers LP tokens from one address to another
     /// @dev if 'to' is the address(0), it will burn the tokens instead of transferring.
+    /// @param from The address to transfer tokens from
+    /// @param to The address to transfer tokens to
+    /// @param poolId The poolId of the token
+    /// @param longX The amount of long position X tokens to transfer
+    /// @param shortX The amount of short position X tokens to transfer
+    /// @param longY The amount of long position Y tokens to transfer
+    /// @param shortY The amount of short position Y tokens to transfer
+    /// @return A boolean indicating whether the transfer succeeded
     function transferFrom(
         address from,
         address to,
@@ -145,8 +180,10 @@ abstract contract Liquidity is ILiquidity, Deadline {
         uint128 shortY
     ) public returns (bool) {
         require(to != address(this), InvalidAddress());
+        // Check TTL allowance.
         require(allowance[from][msg.sender][poolId] >= block.timestamp, InsufficientAllowance());
         if (to != address(0)) {
+            // Similar logic to transfer...
             LpInfo storage lpInfo_from = _balances[from][poolId];
             lpInfo_from.longX -= longX;
             lpInfo_from.shortX -= shortX;
@@ -175,6 +212,15 @@ abstract contract Liquidity is ILiquidity, Deadline {
     //////////////////////////////////////////////////////////////*/
 
     /// @notice Approves a spender to transfer tokens using a signature
+    /// @param owner The address of the token owner
+    /// @param spender The address to approve or revoke permission for
+    /// @param poolId The poolId of the token
+    /// @param value The block timestamp limit
+    /// @param deadline The time at which the signature expires
+    /// @param v The recovery byte of the signature
+    /// @param r Half of the ECDSA signature pair
+    /// @param s Half of the ECDSA signature pair
+    /// @return A boolean indicating whether the operation succeeded
     function permit(
         address owner,
         address spender,
@@ -188,6 +234,7 @@ abstract contract Liquidity is ILiquidity, Deadline {
         // Unchecked because the only math done is incrementing
         // the owner's nonce which cannot realistically overflow.
         unchecked {
+            // Recover signer and validate.
             address recoveredAddress = ecrecover(
                 keccak256(
                     abi.encodePacked(
@@ -222,6 +269,7 @@ abstract contract Liquidity is ILiquidity, Deadline {
     }
 
     /// @notice Returns the domain separator used in the permit signature
+    /// @return The domain separator
     function DOMAIN_SEPARATOR() public view override returns (bytes32) {
         return block.chainid == INITIAL_CHAIN_ID ? INITIAL_DOMAIN_SEPARATOR : computeDomainSeparator();
     }
@@ -244,13 +292,21 @@ abstract contract Liquidity is ILiquidity, Deadline {
     //////////////////////////////////////////////////////////////*/
 
     /// @notice Mints new LP tokens to an address
+    /// @param to Recipient of the minted tokens
+    /// @param poolId The pool ID
+    /// @param longX Amount of long X to mint
+    /// @param shortX Amount of short X to mint
+    /// @param longY Amount of long Y to mint
+    /// @param shortY Amount of short Y to mint
     function _mint(address to, uint256 poolId, uint128 longX, uint128 shortX, uint128 longY, uint128 shortY) internal {
+        // Update total supply.
         LpInfo storage lpInfo_total = _totalSupply[poolId];
         lpInfo_total.longX += longX;
         lpInfo_total.shortX += shortX;
         lpInfo_total.longY += longY;
         lpInfo_total.shortY += shortY;
 
+        // Update balance (unchecked for overflow safety).
         LpInfo storage lpInfo_bal = _balances[to][poolId];
         // Cannot overflow because the sum of all user
         // balances can't exceed the max uint256 value.
@@ -265,15 +321,23 @@ abstract contract Liquidity is ILiquidity, Deadline {
     }
 
     /// @notice Burns LP tokens from an address
+    /// @param from Address to burn from
+    /// @param poolId The pool ID
+    /// @param longX Amount of long X to burn
+    /// @param shortX Amount of short X to burn
+    /// @param longY Amount of long Y to burn
+    /// @param shortY Amount of short Y to burn
     function _burn(address from, uint256 poolId, uint128 longX, uint128 shortX, uint128 longY, uint128 shortY)
         internal
     {
+        // Update balance.
         LpInfo storage lpInfo_bal = _balances[from][poolId];
         lpInfo_bal.longX -= longX;
         lpInfo_bal.shortX -= shortX;
         lpInfo_bal.longY -= longY;
         lpInfo_bal.shortY -= shortY;
 
+        // Update total supply (unchecked for underflow safety).
         LpInfo storage lpInfo_total = _totalSupply[poolId];
         // Cannot underflow because a user's balance
         // will never be larger than the total supply.
