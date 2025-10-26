d4e44f93c8282e799122df9c9b7b06d796f414c4
Settee <ake14243@gmail.com>
2025-10-26 13:45:04 +0700

    remove old getPairId

--- src/libraries/PairLibrary.sol
+++ src/libraries/PairLibrary.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.30;
 import {IERC20} from "../interfaces/IERC20.sol";
 
 library PairLibrary {
-    function computePairId(address token0, address token1) internal pure returns (uint256 pairId) {
-        pairId = uint256(keccak256(abi.encodePacked(token0, token1)));
-    }
+    // function computePairId(address token0, address token1) internal pure returns (uint256 pairId) {
+    //     pairId = uint256(keccak256(abi.encodePacked(token0, token1)));
+    // }
 
     function getBalance(address token) internal view returns (uint256 balance) {
         balance = token == address(0) ? address(this).balance : IERC20(token).balanceOf(address(this));
