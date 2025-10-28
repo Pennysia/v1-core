991686dae5517d4083a36e3ef82e89d5a87320e6
Settee <ake14243@gmail.com>
2025-10-28 23:13:35 +0700

    create constant file

---
+++ src/libraries/Constant.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity 0.8.30;
+
+library Constant {
+    uint256 internal constant MINIMUM_LIQUIDITY = 3000;
+    uint256 internal constant SCALE = 1 << 128;
+}
