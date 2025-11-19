583f3bff51943a5d2cef314a62e07128ea4440c1
Settee <99028384+Settee-ake@users.noreply.github.com>
2025-11-19 22:57:05 +0700

    rename 'to' to 'payer'

--- src/interfaces/IPayment.sol
+++ src/interfaces/IPayment.sol
@@ -5,7 +5,6 @@ pragma solidity 0.8.30;
 ///@notice these are payment request callbacks covering mint, burn, flash, and swap
 ///@dev for direct pool interaction, this interface is not needed. see [Market.sol] for more details.
 interface IPayment {
-    /// @notice this is used in createLiquidity, flash, and swap
-    /// the caller need to pays specific amounts of tokens to the Market contract.
-    function requestToken(address to, address[] memory tokens, uint256[] memory paybackAmounts) external payable;
+    /// @notice the caller need to pays specific amounts of tokens to the Market contract.
+    function requestToken(address payer, address[] memory tokens, uint256[] memory paybackAmounts) external payable;
 }
